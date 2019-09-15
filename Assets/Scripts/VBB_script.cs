bb716dfe5e3173080c8ef14f835c2f1e15b03a96
Edward <ereyes28@uic.edu>
2019-09-14 20:47:07 -0500

    added Voice clip

---
+++ Assets/Scripts/VBB_script.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class VBB_script: MonoBehaviour, IVirtualButtonEventHandler
+{
+
+    public GameObject vbBtnObj;
+    public Animator cubeAni;
+    public GameObject gandalf;
+
+    // Use this for initialization
+    void Start()
+    {
+        vbBtnObj = GameObject.Find("You Shall Not Pass!");
+        
+        vbBtnObj.GetComponent<VirtualButtonBehaviour>().RegisterEventHandler(this);
+        //cubeAni.GetComponent<Animator>();
+    }
+
+    public void OnButtonPressed(VirtualButtonBehaviour vb)
+    {
+        Debug.Log("Button pressed");
+        gandalf.SetActive(true);
+    }
+
+    public void OnButtonReleased(VirtualButtonBehaviour vb)
+    {
+        Debug.Log("Button released");
+    }
+}
