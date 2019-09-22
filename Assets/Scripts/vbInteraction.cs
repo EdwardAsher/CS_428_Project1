cdcf25b90a91a47231f5a654245bf2b8b635fe2b
jonelalcasid <jonelalcasid@yahoo.com>
2019-09-22 10:41:58 -0500

    Update vbInteraction.cs Script

--- Assets/Scripts/vbInteraction.cs
+++ Assets/Scripts/vbInteraction.cs
@@ -9,16 +9,22 @@ public class vbInteraction : MonoBehaviour, IVirtualButtonEventHandler
     VirtualButtonBehaviour[] virtualButtonBehaviours;
 
     string vbName;
-    public GameObject VideoSurface_YSNP, VideoSurface_jalcas2, VideoSurface_ereyes28;
-    public GameObject vbBtnObj;
+    public GameObject VideoSurface_YSNP, VideoSurface_jalcas2, VideoSurface_ereyes28, ReviewSurface_jalcas2, ReviewSurface_ereyes28;
+    public GameObject VideoBtnObj;
+    public GameObject ReviewBtnObj;
     int count = 0;
+    int count2 = 0;
 
     // Start is called before the first frame update
     void Start()
     {
-        vbBtnObj = GameObject.Find("VideoButtonPlayer");
+        VideoBtnObj = GameObject.Find("VideoButtonPlayer");
 
-        vbBtnObj.GetComponent<VirtualButtonBehaviour>().RegisterEventHandler(this);
+        VideoBtnObj.GetComponent<VirtualButtonBehaviour>().RegisterEventHandler(this);
+
+        ReviewBtnObj = GameObject.Find("ReviewButtonPlayer");
+
+        ReviewBtnObj.GetComponent<VirtualButtonBehaviour>().RegisterEventHandler(this);
         //rtualButtonBehaviours = GetComponentsInParent<VirtualButtonBehaviour>();
 
         //r(int i = 0; i < virtualButtonBehaviours.Length; i++)
@@ -29,45 +35,80 @@ public class vbInteraction : MonoBehaviour, IVirtualButtonEventHandler
 
     public void OnButtonPressed(VirtualButtonBehaviour vb)
     {
+        vbName = vb.VirtualButtonName;
+        if(vbName == "VideoButtonPlayer")
+        {
+            VideoButtonPlayer();
+        }
+        if(vbName == "ReviewButtonPlayer")
+        {
+            ReviewButtonPlayer();
+        }
+    }
+
+    public void OnButtonReleased(VirtualButtonBehaviour vb)
+    {
+        
+    }
+
+    void VideoButtonPlayer()
+    {
         if (count == 0)
         {
             VideoSurface_YSNP.SetActive(true);
             VideoSurface_jalcas2.SetActive(false);
             VideoSurface_ereyes28.SetActive(false);
+            ReviewSurface_jalcas2.SetActive(false);
+            ReviewSurface_ereyes28.SetActive(false);
         }
-        else if(count == 1)
+        else if (count == 1)
         {
             VideoSurface_YSNP.SetActive(false);
             VideoSurface_jalcas2.SetActive(true);
             VideoSurface_ereyes28.SetActive(false);
+            ReviewSurface_jalcas2.SetActive(false);
+            ReviewSurface_ereyes28.SetActive(false);
         }
-        else if(count == 2)
+        else if (count == 2)
         {
             VideoSurface_YSNP.SetActive(false);
             VideoSurface_jalcas2.SetActive(false);
             VideoSurface_ereyes28.SetActive(true);
+            ReviewSurface_jalcas2.SetActive(false);
+            ReviewSurface_ereyes28.SetActive(false);
         }
         else
         {
             count = -1;
         }
         count++;
-         
-        //Name = vb.VirtualButtonName;
-        //(vbName == "VideoButtonPlayer")
-        //
-        //  VideoButtonPlayer();
-        //
+        Debug.Log("Button pressed");
     }
 
-    public void OnButtonReleased(VirtualButtonBehaviour vb)
+    void ReviewButtonPlayer()
     {
-
-    }
-
-    void VideoButtonPlayer()
-    {
-        //deoSurface_YSNP.SetActive(true);
+        if(count2 == 0)
+        {
+            ReviewSurface_jalcas2.SetActive(true);
+            ReviewSurface_ereyes28.SetActive(false);
+            VideoSurface_YSNP.SetActive(false);
+            VideoSurface_jalcas2.SetActive(false);
+            VideoSurface_ereyes28.SetActive(false);
+        }
+        else if(count2 == 1)
+        {
+            ReviewSurface_jalcas2.SetActive(false);
+            ReviewSurface_ereyes28.SetActive(true);
+            VideoSurface_YSNP.SetActive(false);
+            VideoSurface_jalcas2.SetActive(false);
+            VideoSurface_ereyes28.SetActive(false);
+        }
+        else
+        {
+            count2 = -1;
+        }
+        count2++;
+        Debug.Log("Button pressed");
     }
 
     // Update is called once per frame
